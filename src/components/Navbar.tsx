4c4a6bde48c17293d24ebab4a0b5f03bff2c9ecf
Elsayed Abdullah <sayedaladway@gmail.com>
2024-03-09 11:23:25 +0200

    fix light mode design isuues

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,27 +12,23 @@ const Navbar = () => {
     location.replace("/login");
   };
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-white border-gray-200 ">
       <div className="max-w-screen-lg flex flex-wrap items-center justify-between mx-auto p-4">
         <Link
           to="/"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
-          <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center text-lg font-semibold whitespace-nowrap">
             Todo App
           </span>
         </Link>
         {userData ? (
           <div className="flex relative items-center md:order-2 space-x-5 rtl:space-x-reverse">
-            <NavLink className="text-white" to={"/todos"}>
-              Todos
-            </NavLink>
-            <NavLink className="text-white" to={"/profile"}>
-              Profile
-            </NavLink>
+            <NavLink to={"/todos"}>Todos</NavLink>
+            <NavLink to={"/profile"}>Profile</NavLink>
             <button
               type="button"
-              className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+              className="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300"
               id="user-menu-button"
               aria-expanded="false"
               data-dropdown-toggle="user-dropdown"
@@ -49,14 +45,14 @@ const Navbar = () => {
 
             {isOpen && (
               <div
-                className="z-50 absolute right-0 top-8 left-auto my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                className="z-50 absolute right-0 top-8 left-auto my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow"
                 id="user-dropdown"
               >
                 <div className="px-4 py-3">
-                  <span className="block text-sm text-gray-900 dark:text-white">
+                  <span className="block text-sm text-gray-900">
                     {userData.user.username}
                   </span>
-                  <span className="block text-sm  text-gray-500 truncate dark:text-gray-400">
+                  <span className="block text-sm  text-gray-500 truncate">
                     {userData.user.email}
                   </span>
                 </div>
@@ -64,7 +60,7 @@ const Navbar = () => {
                   <li>
                     <button
                       onClick={logout}
-                      className="w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                      className="w-full block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                     >
                       Sign out
                     </button>
@@ -75,10 +71,10 @@ const Navbar = () => {
           </div>
         ) : (
           <ul className="flex items-center space-x-3">
-            <li className="text-white duration-200 font-semibold text-base">
+            <li className="duration-200 font-semibold text-base">
               <NavLink to="/register">Register</NavLink>
             </li>
-            <li className="text-white duration-200 font-semibold text-base">
+            <li className="duration-200 font-semibold text-base">
               <NavLink to="/login">Login</NavLink>
             </li>
           </ul>
